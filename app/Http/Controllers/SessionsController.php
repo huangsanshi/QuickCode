c4cb2846ddc2046a49dc11f77e36a4ea52e712df
huangsanshi <1204807266@qq.com>
2018-08-08 18:51:48 +0800

    退出

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,7 +20,7 @@ class SessionsController extends Controller
                 'password' => 'required',
             ]);
 
-        if(Auth::attempt($vali)){
+        if(Auth::attempt($vali,$request->has('remember'))){
             session()->flash('success','登录成功！');
             return redirect()->route('users.show',[Auth::user()]);
         }else{
@@ -28,5 +28,13 @@ class SessionsController extends Controller
             return redirect()->back();
         }
         return ;
+    }
+
+    //退出登录
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','您已成功退出!');
+        return redirect('login');
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,13 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+                'except' => ['show','create','store']
+            ]);
+    }
+
     //注册
     public function create()
     {
@@ -37,5 +44,30 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    //编辑页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //编辑更新
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request,[
+                'name' => 'required|max:50',
+                'password' => 'nullable|confirmed|min:6',
+            ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+        session()->flash('success','个人资料更新成功!');
+        return redirect()->route('users.show',$user->id);
     }
 }
