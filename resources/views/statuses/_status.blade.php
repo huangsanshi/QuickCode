33c3a0a5f5e01c54dc956915bb358a34c0314460
huangsanshi <1204807266@qq.com>
2018-09-12 18:04:20 +0800

    显示微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,6 +1,6 @@
 <li id="status-{{ $status->id }}">
   <a href="{{ route('users.show', $user->id )}}">
-    <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar"/>
+    <img src="https://pic.qqtn.com/up/2018-7/2018072918092112846.jpg" alt="{{ $user->name }}" style="height: 50px;width: 50px;" />
   </a>
   <span class="user">
     <a href="{{ route('users.show', $user->id )}}">{{ $user->name }}</a>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,7 +1,56 @@
 @extends('layouts.default')
 @section('title',$user->name)
+@section('style')
+<style type="text/css">
+/* statuses */
+  .user {
+    margin-top: 5em;
+    padding-top: 0;
+  }
+    .content {
+    display: block;
+    margin-left: 60px;
+    word-break: break-word;
+  }
+    .timestamp {
+    color: $gray-light;
+    display: block;
+    margin-left: 60px;
+  }
+
+  .gravatar {
+    margin-right: 10px;
+    margin-top: 5px;
+  }
+.statuses {
+  list-style: none;
+  padding: 0;
+  margin-top: 20px;
+}
+    button.status-delete-btn {
+      position: absolute;
+      right: 0;
+      top: 10px;
+    }
+  li {
+    padding: 10px 0;
+    border-top: 1px solid #e8e8e8;
+    position: relative;
+  }
+aside {
+  textarea {
+    height: 100px;
+    margin-bottom: 5px;
+  }
+}
+
+.status_form {
+  margin-top: 20px;
+}
+</style>
+@stop
 @section('content')
-<div class="row">
+<div class="row top">
   <div class="col-md-offset-2 col-md-8">
     <div class="col-md-12">
       <div class="col-md-offset-2 col-md-8">
